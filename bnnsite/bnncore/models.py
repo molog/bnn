b8d2fa0ac4c538bc483b90dd1f743cf6b2d9ac87
hbao <hbao@localhost>
2013-05-31 09:56:21 +0800

    better display

--- bnnsite/bnncore/models.py
+++ bnnsite/bnncore/models.py
@@ -10,7 +10,10 @@ class PaymentSlip(models.Model):
     total_amount = models.DecimalField(max_digits=6, decimal_places=2)
     paid_date = models.DateField(default=datetime.date.today())
     created_date = models.DateTimeField(auto_now_add=True, editable=False)
-    notes = models.TextField(blank=True)
+    notes = models.TextField()
+    
+    class Meta:
+        ordering = ['-paid_date', '-id']
     
     def __unicode__(self):
         return u'%s %s ￥%s' % (self.user, self.paid_date, self.total_amount)
