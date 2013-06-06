3a9abbbe90c37f7c584045d510047921fa78ac69
hbao <hbao@localhost>
2013-06-06 11:51:50 +0800

    add profile and house model

--- bnnsite/bnncore/models.py
+++ bnnsite/bnncore/models.py
@@ -4,9 +4,20 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# Create your models here.
+
+class House(models.Model):
+    name = models.CharField(max_length=100)
+    
+    
+class BnnProfile(models.Model):
+    user = models.OneToOneField(User)
+    house = models.ForeignKey(House)
+    nick_name = models.CharField(max_length=100, blank=True)
+    
+
 class PaymentSlip(models.Model):
     user = models.ForeignKey(User)
+    profile = models.ForeignKey(BnnProfile, null=True)
     total_amount = models.DecimalField(max_digits=6, decimal_places=2)
     paid_date = models.DateField(default=datetime.date.today())
     created_date = models.DateTimeField(auto_now_add=True, editable=False)
@@ -17,6 +28,7 @@ class PaymentSlip(models.Model):
     
     def __unicode__(self):
         return u'%s %s ￥%s' % (self.user, self.paid_date, self.total_amount)
+
 
 
 
