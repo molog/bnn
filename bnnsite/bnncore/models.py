22939d6216ebd443827c53ca78a33e1e46ff5c5d
hbao <hbao@localhost>
2013-06-06 12:05:20 +0800

    add admin display

--- bnnsite/bnncore/models.py
+++ bnnsite/bnncore/models.py
@@ -8,12 +8,17 @@ from django.contrib.auth.models import User
 class House(models.Model):
     name = models.CharField(max_length=100)
     
+    def __unicode__(self):
+        return self.name
+    
     
 class BnnProfile(models.Model):
     user = models.OneToOneField(User)
     house = models.ForeignKey(House)
     nick_name = models.CharField(max_length=100, blank=True)
     
+    def __unicode__(self):
+        return self.nick_name if self.nick_name else self.user.__unicode__()
 
 class PaymentSlip(models.Model):
     user = models.ForeignKey(User)
@@ -27,11 +32,13 @@ class PaymentSlip(models.Model):
         ordering = ['-paid_date', '-id']
     
     def __unicode__(self):
-        return u'%s %s ￥%s' % (self.user, self.paid_date, self.total_amount)
+        return u'%s %s ￥%s' % (self.profile, self.paid_date, self.total_amount)
 
 
 
 
 from django.contrib import admin
 
+admin.site.register(House)
+admin.site.register(BnnProfile)
 admin.site.register(PaymentSlip)
